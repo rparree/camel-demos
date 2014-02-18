eaf316245fc00d2cce2c087b6265b9c8bcf4dcf2
Raphael Parree <rparree@edc4it.com>
2014-02-18 10:16:50 +0100

    Renamed the packages and files according to the changed demo name

--- core-camel/src/main/scala/demo/core/CoreDemoRouteBuilder.scala
+++ core-camel/src/main/scala/demo/core/CoreDemoRouteBuilder.scala
@@ -1,17 +1,15 @@
-package demo.file
+package demo.core
 
 import org.apache.camel.scala.dsl.builder.RouteBuilder
 
 /**
  * todo  
  */
-class FileDemoRouteBuilder extends RouteBuilder {
+class CoreDemoRouteBuilder extends RouteBuilder {
 
   "file:./target/scala-2.10/classes/camel/in" ==> {
-    threads (5){
       log("received file ${file:name}")
-      --> ("class:demo.file.HeavyBean?method=workHard")
-    }
+      --> ("class:demo.core.HeavyBean?method=workHard")
 
   }
 
