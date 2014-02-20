f3a8ee2a02863cf42887e83d3e2566a12ef16883
Raphael Parree <rparree@edc4it.com>
2014-02-20 08:12:47 +0100

    added netty-seda-inout example

---
+++ seda-inout/src/main/scala/demo/seda/SedaDemo.scala
@@ -0,0 +1,26 @@
+package demo.seda
+
+import java.nio.file.{FileSystems, Files}
+import org.apache.camel.impl.DefaultCamelContext
+import org.apache.camel.main.Main
+
+
+/**
+ * todo
+ */
+object SedaDemo extends  App {
+
+
+  val m = new Main
+  val context = new DefaultCamelContext()
+  //context.addComponent("netty",)
+  m.addRouteBuilder(new org.apache.camel.scala.dsl.builder.RouteBuilder(){
+    "netty:tcp://0.0.0.0:7090?textline=true&sync=true" log "received ${body}" to "seda:a"
+
+    "seda:a" log "received ${body}" process(e => e.getOut.setBody("Some Result from seda route\n"))
+  }.builder)
+
+  m.enableHangupSupport()
+  m.run()
+
+}
