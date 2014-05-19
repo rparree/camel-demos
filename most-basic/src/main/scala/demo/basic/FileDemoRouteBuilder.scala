af2e2b315be8cd16972bd15d5652fa0aa653ea9b
Raphael Parree <rparree@edc4it.com>
2014-05-19 14:30:01 +0100

    Made the first demo a bit more interesting (and contains a "to")

--- most-basic/src/main/scala/demo/basic/FileDemoRouteBuilder.scala
+++ most-basic/src/main/scala/demo/basic/FileDemoRouteBuilder.scala
@@ -7,7 +7,11 @@ import org.apache.camel.scala.dsl.builder.RouteBuilder
  */
 class FileDemoRouteBuilder extends RouteBuilder {
 
-  "file:target/scala-2.10/classes/camel/in?move=processed"  log ("received file ${file:name}") 
+  "file:target/scala-2.10/classes/camel/in?move=processed"  ==>{
+    log ("received file ${file:name}") 
+    transform( xpath("/employee/email/text()") )
+    --> ("file:target/scala-2.10/classes/camel/out")
+  } 
 
 
 }
